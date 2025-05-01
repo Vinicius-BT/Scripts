08769f6efe234a0ec9cc413a056d640f73cda3bb
Vinicius-BT <82327452+Vinicius-BT@users.noreply.github.com>
2025-05-01 20:18:14 -0300

    Update Deepnote.js

--- Deepnote.js
+++ Deepnote.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Deepnote Auto Click Delay 🖱
+// @name         Deepnote Auto Click Delay
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.0.0
 // @description  Clica no botão "Delay shutdown by 60 minutes", mostra notificação e toca som do Verstappen
 // @match        https://deepnote.com/*
 // @icon         https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://deepnote.com&size=64
