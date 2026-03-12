5d9b0e97ae979cda58dc60069eb5ed6724b595cf
Vinicius-BT <82327452+Vinicius-BT@users.noreply.github.com>
2026-03-12 16:38:11 -0300

    Update user script name and version to 1.3
    Updated version and name for the user script.

--- Gravure Idol Video Blog sites.user.js
+++ Gravure Idol Video Blog sites.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
-// @name         Gravure Idols ferramentas unificadas (v1.2)
+// @name         Gravure Idols ferramentas unificadas
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  Correção definitiva para códigos complexos, IDs numéricos (ex: 211880) e detecção de títulos
+// @icon         https://baseec-img-mng.akamaized.net/images/user/logo/dde8cbaba8f25c311920bd2e0e13afd6.png
 // @author       Gemini
 // @match        *://youiv.tv/*
 // @match        *://ivworld.net/*
